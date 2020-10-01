a1a12a76fd8d713c9904f4e664dee587fe8edd43
Maurice Pasternak <maurice.pasternak@utoronto.ca>
2020-09-30 23:55:36 -0400

    Plotting updates around MRI Viewer: 1) Column dtypes can be inferred somewhat by their name (i.e sex/gender will be immediately assumed to be categorical) 2) MRI Viewer now has some preliminary cmaps added. Will have to fix array values, as they do not seem to be accurate representations of CBF values. 3) Some tooltips added to the MRI Viewer

--- ExploreASL_GUI/xASL_GUI_Graph_MRIViewManager.py
+++ ExploreASL_GUI/xASL_GUI_Graph_MRIViewManager.py
@@ -84,6 +84,7 @@ class xASL_GUI_MRIViewManager(QWidget):
         self.artist: xASL_GUI_MRIViewArtist = self.parent_cw.fig_artist
         if self.artist is not None:
             self.cmb_selectsubject.currentTextChanged.connect(self.artist.switch_subject)
+            self.cmb_cmap.currentTextChanged.connect(self.artist.plotupdate_allslices)
             self.slider_axialslice.valueChanged.connect(self.artist.plotupdate_axialslice)
             self.slider_coronalslice.valueChanged.connect(self.artist.plotupdate_coronalslice)
             self.slider_sagittalslice.valueChanged.connect(self.artist.plotupdate_sagittalslice)
@@ -98,19 +99,28 @@ class xASL_GUI_MRIViewManager(QWidget):
         if plot_type == "Strip Plot":
             self.plotting_func = sns.stripplot
             self.le_x = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, self.categorical_dtypes)
+            self.le_x.setToolTip("Indicate the Categorical variable that should be on the x-axis")
             self.le_x.setPlaceholderText("Drag & Drop the X-axis Variable")
             self.le_y = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, self.numeric_dtypes)
+            self.le_y.setToolTip("Indicate the Numberical variable that should be on the y-axis")
             self.le_y.setPlaceholderText("Drag & Drop the Y-axis Variable")
             self.le_hue = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, ["object", "category"])
+            self.le_hue.setToolTip("Indicate the Categorical variable by which data should be grouped\n "
+                                   "using marker color differences")
             self.le_hue.setPlaceholderText("Drag & Drop the Hue Grouping Variable")
             self.axes_arg_x = self.le_x.text
             self.axes_arg_y = self.le_y.text
             self.axes_arg_hue = self.le_hue.text
 
             self.dodge = QCheckBox(checked=True)
+            self.dodge.setToolTip("Indicate whether, when grouping by a Hue variable, the stripplot should feature\n"
+                                  "separate columns of points for the different levels within the Hue variable category")
             self.size = QDoubleSpinBox(maximum=10, minimum=1, value=4.5, singleStep=0.1)
+            self.size.setToolTip("Indicate the radius of the points. Units are arbitary.")
             self.linewidth = QDoubleSpinBox(maximum=2, minimum=0, value=0.1, singleStep=0.1)
+            self.linewidth.setToolTip("Indicate the thickness of the outline around points. Units are arbitary")
             self.cmb_palette = self.UI_Setup_PaletteCombobox()
+            self.cmb_palette.setToolTip("Indicate the color palette that should be used")
             self.axes_kwargs = self.UI_Setup_AxesMappings(["dodge", "size", "linewidth", "palette"],
                                                           [self.dodge, self.size, self.linewidth, self.cmb_palette])
             # Create the underlying widget container and form layout
@@ -129,21 +139,31 @@ class xASL_GUI_MRIViewManager(QWidget):
         elif plot_type == "Scatter Plot":
             self.plotting_func = sns.scatterplot
             self.le_x = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, self.numeric_dtypes)
+            self.le_x.setToolTip("Indicate the Categorical variable that should be on the x-axis")
             self.le_x.setPlaceholderText("Drag & Drop the X-axis Variable")
             self.le_y = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, self.numeric_dtypes)
+            self.le_y.setToolTip("Indicate the Numberical variable that should be on the y-axis")
             self.le_y.setPlaceholderText("Drag & Drop the Y-axis Variable")
             self.le_hue = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, ["object", "category"])
+            self.le_hue.setToolTip("Indicate the Categorical variable by which data should be grouped\n "
+                                   "using marker color differences")
             self.le_hue.setPlaceholderText("Drag & Drop the Hue Grouping Variable")
             self.axes_arg_x = self.le_x.text
             self.axes_arg_y = self.le_y.text
             self.axes_arg_hue = self.le_hue.text
 
             self.size_grouper = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, self.all_dtypes)
+            self.size_grouper.setToolTip("Indicate the variable by which data should be grouped\n"
+                                         "using marker size differences")
             self.size_grouper.setPlaceholderText("(Optional) Drag & Drop the Markersize-Grouping Variable")
             self.style_grouper = DandD_Graphing_ListWidget2LineEdit(self.parent_cw, ["object", "category"])
+            self.style_grouper.setToolTip("Indicate the variable by which data should be grouped\n"
+                                          "using marker style differences")
             self.style_grouper.setPlaceholderText("(Optional) Drag & Drop the Markerstyle-Grouping Variable")
             self.cmb_palette = self.UI_Setup_PaletteCombobox()
+            self.cmb_palette.setToolTip("Indicate the color palette that should be used")
             self.spinbox_markersize = QDoubleSpinBox(maximum=100, minimum=0, value=40, singleStep=1)
+            self.spinbox_markersize.setToolTip("Indicate the radius of the points. Units are arbitary.")
             self.axes_kwargs = self.UI_Setup_AxesMappings(["size", "style", "palette", "s"],
                                                           [self.size_grouper, self.style_grouper,
                                                            self.cmb_palette, self.spinbox_markersize])
@@ -177,11 +197,14 @@ class xASL_GUI_MRIViewManager(QWidget):
         self.slider_axialslice = QSlider(Qt.Horizontal, minimum=0, maximum=120, singleStep=1, value=50)
         self.slider_coronalslice = QSlider(Qt.Horizontal, minimum=0, maximum=144, singleStep=1, value=50)
         self.slider_sagittalslice = QSlider(Qt.Horizontal, minimum=0, maximum=120, singleStep=1, value=50)
+        self.cmb_cmap = QComboBox()
+        self.cmb_cmap.addItems(["gray", "inferno", "nipy_spectral", "gnuplot2"])
 
         self.formlay_mriparms.addRow("Subject_Run Selection", self.cmb_selectsubject)
         self.formlay_mriparms.addRow("Axial Slice", self.slider_axialslice)
         self.formlay_mriparms.addRow("Coronal Slice", self.slider_coronalslice)
         self.formlay_mriparms.addRow("Sagittal Slice", self.slider_sagittalslice)
+        self.formlay_mriparms.addRow("Colormap", self.cmb_cmap)
 
     # Convenience Function - clears the Axes Parameters Tab
     # noinspection PyTypeChecker
